84834ed256c07d1883f0a46ca03c35b62e3e4e73
Saahmuael <zhang.j.sam@gmail.com>
2025-10-21 21:34:45 -0400

    Test stuff

---
+++ serializer_test/test.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+import random
+
+@cocotb.test()
+async def test_project(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut._log.info("Reset")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    dut._log.info("Test project behavior")
+
+    #TEST CONFIGS==========
+    numberCycles = 10 #Test how many times
+    #=======================
+    for _ in range(numberCycles):
+        if random.randint(0,9) < 6: #Send something
+            pass
+        else: #Do nothing
+            pass
