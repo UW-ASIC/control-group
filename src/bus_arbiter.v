c2d1c188175ef1f97cc922cde22177f7a1496cf7
Ernest Tang <e42tang@uwaterloo.ca>
2025-11-17 15:35:21 -0500

    Improved arbiter logic and implemented bus_ready input

--- src/bus_arbiter.v
+++ src/bus_arbiter.v
@@ -8,16 +8,68 @@ module bus_arbiter #(
     input wire aes_req,
     input wire [ADDRW+7:0] sha_data_in,
     input wire [ADDRW+7:0] aes_data_in,
+    input wire bus_ready,
 
     output reg [7:0] data_out,
+    output reg valid_out, 
     output wire aes_grant,
     output wire sha_grant
 );
 
+localparam AES = 2'b01;
+localparam SHA = 2'b10;
 
 reg last_serviced; // RR to choose a FSM to service if both simultaneously req bus
 reg [1:0] curr_mode; // 00: Inactive, 01: AES, 10: SHA
 reg [1:0] counter;
+
+always @(*) begin
+    if (counter == 2'b00) begin
+        if (curr_mode == AES) begin
+            data_out = aes_data_in[7:0];
+            valid_out = 1'b1;
+        end else if (curr_mode == SHA) begin
+            data_out = sha_data_in[7:0];
+            valid_out = 1'b1;
+        end else begin
+            data_out = 8'b0; // Don't care
+            valid_out = 1'b0;
+        end
+    end else if (counter == 2'b01) begin
+        if (curr_mode == AES) begin
+            data_out = aes_data_in[15:8];
+            valid_out = 1'b1;
+        end else if (curr_mode == SHA) begin
+            data_out = sha_data_in[15:8];
+            valid_out = 1'b1;
+        end else begin
+            data_out = 8'b0; // Don't care
+            valid_out = 1'b0;
+        end
+    end else if (counter == 2'b10) begin
+        if (curr_mode == AES) begin
+            data_out = aes_data_in[23:16];
+            valid_out = 1'b1;
+        end else if (curr_mode == SHA) begin
+            data_out = sha_data_in[23:16];
+            valid_out = 1'b1;
+        end else begin
+            data_out = 8'b0; // Don't care
+            valid_out = 1'b0;
+        end
+    end else begin
+        if (curr_mode == AES) begin
+            data_out = aes_data_in[31:24];
+            valid_out = 1'b1;
+        end else if (curr_mode == SHA) begin
+            data_out = sha_data_in[31:24];
+            valid_out = 1'b1;
+        end else begin
+            data_out = 8'b0; // Don't care
+            valid_out = 1'b0;
+        end
+    end
+end
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
@@ -25,55 +77,31 @@ always @(posedge clk or negedge rst_n) begin
         curr_mode <= 2'b00;
         counter <= 2'b00;
     end else begin
-        if (curr_mode == 2'b00) begin
-            if (sha_req && aes_req) begin
-                if (last_serviced) begin
-                    // Service SHA 
-                    curr_mode <= 2'b10;
-                end else begin
-                    // Service AES
-                    curr_mode <= 2'b01;
-                end
-            end else if (aes_req) begin 
+        if (curr_mode != 2'b00) begin
+            counter <= counter + 1;
+        end else begin
+            // Counter should always be 0 when curr_mode == 2'b00
+            if (sha_req && aes_req && bus_ready) begin 
+                curr_mode <= last_serviced ? 2'b10 : 2'b01;
+            end else if (aes_req && bus_ready) begin 
                 curr_mode <= 2'b01;
-            end else if (sha_req) begin
+            end else if (sha_req && bus_ready) begin
                 curr_mode <= 2'b10;
             end
-            counter <= 2'b00;
-        end else if (curr_mode == 2'b01) begin
-            if (counter == 2'b00) begin
-                data_out <= aes_data_in[7:0];
-            end else if (counter == 2'b01) begin
-                data_out <= aes_data_in[15:8];
-            end else if (counter == 2'b10) begin
-                data_out <= aes_data_in[23:16];
-            end else begin
-                data_out <= aes_data_in[31:24];
-                if (sha_req) curr_mode <= 2'b10;
-                else begin
-                    curr_mode <= 2'b00;
-                    last_serviced <= 1'b1;
-                end
-            end
-            counter <= counter + 1;
-        end else if (curr_mode == 2'b10) begin
-            if (counter == 2'b00) begin
-                data_out <= sha_data_in[7:0];
-            end else if (counter == 2'b01) begin
-                data_out <= sha_data_in[15:8];
-            end else if (counter == 2'b10) begin
-                data_out <= sha_data_in[23:16];
-            end else begin
-                data_out <= sha_data_in[31:24];
-                last_serviced <= 1'b0;
-            end
-            counter <= counter + 1;
         end
+
+        if (counter == 2'b11) begin
+            if (curr_mode == AES) curr_mode <= (sha_req && bus_ready) ? SHA : 2'b00;
+            else if (curr_mode == SHA) curr_mode <= (aes_req && bus_ready) ? AES : 2'b00;
+        end
+
+        if (curr_mode == AES) last_serviced <= 1'b1;
+        else if (curr_mode == SHA) last_serviced <= 1'b0;
     end
 end
 
-assign aes_grant = (curr_mode == 2'b01);
-assign sha_grant = (curr_mode == 2'b10);
+assign aes_grant = (curr_mode == AES);
+assign sha_grant = (curr_mode == SHA);
 
 
 endmodule
